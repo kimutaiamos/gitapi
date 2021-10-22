0c2f6cb805c7f89781ce92bfc031c82ee674a6d8
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-22 23:34:21 +0300

    added environment key

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +10,7 @@ export class GithubService {
   repos:any;
   constructor( private http:HttpClient) { }
   getUser(user:any){
-    return this.http.get("https://api.github.com/users/" + user+ "?access_token=" + environment.api_key)
-    .pipe(((response:any)=>response));
+    //return this.http.get("https://api.github.com/users/" + user+ "?access_token=" + environment.api_key)
+   // .pipe(((response:any)=>response));
   }
 }
