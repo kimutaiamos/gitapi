079f0793c10612d2e8ff1f9ea0b096444a102cca
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-22 23:20:18 +0300

    added the get username

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
-
+ username:any;
+ gituser:any;
+ gitRepos:any;
+ getUsername(){
+this.service.getUser(this.username).subscribe(details=>{
+  console.log(details);
+  return this.gituser = details;
+});
+ }
   constructor() { }
 
   ngOnInit(): void {
